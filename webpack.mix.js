5762f1ba22a565b37b62803fc4df026bcb9d3aa6
Davide Casiraghi <davide.casiraghi@agiledrop.com>
2020-10-05 16:56:39 +0200

    Add browser sync to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,18 @@ const mix = require('laravel-mix');
  |
  */
 
+// Compile SCSS and JS
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
     ]);
+
+
+// Sync browser any time something change in compiled css, js or views
+mix.browserSync({
+    proxy: 'laravel_jetstream.test',
+    host: 'laravel_jetstream.test',
+    notify: false,
+    files: ['./public/css/*.css', './**/*.htm', './public/js/*.js'], //target the compiled files
+})
