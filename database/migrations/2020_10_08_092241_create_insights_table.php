cf445539e4bcbd0ca11da6f9b1d680cd47a6d0c8
Davide Casiraghi <davide.casiraghi@agiledrop.com>
2020-10-08 11:44:33 +0200

    Create insight, glossary, tags tables

---
+++ database/migrations/2020_10_08_092241_create_insights_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInsightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('insights', function (Blueprint $table) {
+            $table->id();
+            $table->text('insight');
+            $table->string('term')->nullable();
+            $table->boolean('is_published')->default(false);
+
+            $table->unsignedBigInteger('post_id')->nullable();
+
+            $table->boolean('is_posted_on_facebook')->default(false);
+            $table->boolean('is_posted_on_twitter')->default(false);
+            $table->datetime('published_on_facebook_on')->nullable();
+            $table->datetime('published_on_twitter_on')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('insights');
+    }
+}
