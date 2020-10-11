9433e71127ec4d7f66899d1d7e898b6008f026b1
Davide Casiraghi <davide.casiraghi@agiledrop.com>
2020-10-11 16:12:02 +0200

    Adds a footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,8 @@
         @yield('content')
     </div>
 
+    @include('footer')
+
     {{-- JS --}}
     <script src="{{ asset('js/manifest.js') }}" ></script>
     <script src="{{ asset('js/vendor.js') }}" ></script>
