2762d5491eab1b8258849c00c57cb3d7bee0434c
Davide Casiraghi <davide.casiraghi@agiledrop.com>
2020-10-13 07:42:33 +0200

    Adds tags to blog posts

--- resources/views/partials/home/blog_post.blade.php
+++ resources/views/partials/home/blog_post.blade.php
@@ -1,13 +1,17 @@
 <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+
     <div class="flex-shrink-0">
         <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
     </div>
     <div class="flex-1 bg-white p-6 flex flex-col justify-between">
         <div class="flex-1">
             <p class="text-sm leading-5 font-medium text-indigo-600">
+                @foreach($post->tags()->get() as $tag)
                 <a href="#" class="hover:underline">
-                    {{$post->post_category->name}}
+                    {{--{{$post->post_category->name}}--}}
+                    #{{$tag->tag}}  
                 </a>
+                @endforeach
             </p>
             <a href="#" class="block">
                 <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">
