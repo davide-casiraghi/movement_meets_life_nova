0ced04941ba491cf0a6e867da5fbfb4920a26c68
Davide Casiraghi <davide.casiraghi@agiledrop.com>
2020-10-23 19:16:33 +0200

    Accordion almost done, just missing the counter

--- app/Services/Snippets/Accordion.php
+++ app/Services/Snippets/Accordion.php
@@ -17,26 +17,48 @@ class Accordion {
      */
     public function snippetsToHTML($postBody)
     {
+        $count = 0;
+
         // Load the accordion template
-        $sliderTemplate = "<div class='accordion'>";
-        $sliderTemplate .= '<h3>{SLIDER_TITLE}</h3>';
-        $sliderTemplate .= '<div>{SLIDER_CONTENT}</div>';
-        $sliderTemplate .= '</div>';
+        $sliderTemplate = "<div class='w-full'>";
+        $sliderTemplate .= "<input type='checkbox' name='panel' id='panel-1' class='hidden'>";
+            $sliderTemplate .= "<label for='panel-1' class='relative block bg-black text-white p-4 shadow border-b border-grey'>{SLIDER_TITLE}</label>";
+            $sliderTemplate .= "<div class='accordion__content overflow-hidden bg-grey-lighter'>";
+                $sliderTemplate .= "<h2 class='accordion__header pt-4 pl-4'>Header</h2>";
+                $sliderTemplate .= "<p class='accordion__body p-4' id='panel1'>{SLIDER_CONTENT}</p>";
+            $sliderTemplate .= "</div>";
+        $sliderTemplate .= "</div>";
 
         // Do the replacement if needed
         if (substr_count($postBody, '{slide') > 0) {
+
+            /*
+             * If the post is not wrapped yet with 'textHasAccordion accordion', wrap it.
+             (first accordion found in the page)
+            */
+            if (strpos($postBody, 'textHasAccordion') == false) {
+                $postBody = '<div class="textHasAccordion accordion">'.$postBody.'</div>';
+            }
+
             $regex = "#(?:<p>)?\{slide[r]?=([^}]+)\}(?:</p>)?(.*?)(?:<p>)?\{/slide[r]?\}(?:</p>)?#s";
 
             $postBody = preg_replace(
                 $regex,
                 str_replace(
                     ['{SLIDER_TITLE}', '{SLIDER_CONTENT}'],
-                    ['$1', '$2'],
-                    $sliderTemplate
+                    ['$1', '$2', '$3'],
+                    $sliderTemplate,
                 ),
                 $postBody
             );
         }
         return $postBody;
+    }
+
+
+
+    function rep_count($matches) {
+        global $count;
+        return 'test' . $count++;
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,42 +15,5 @@
         </div>
     </div>
 
-
-
-
-
-    <div class="accordion flex flex-col items-center justify-center h-screen">
-        <!--  Panel 1  -->
-        <div class="w-1/2">
-            <input type="checkbox" name="panel" id="panel-1" class="hidden">
-            <label for="panel-1" class="relative block bg-black text-white p-4 shadow border-b border-grey">Panel 1</label>
-            <div class="accordion__content overflow-hidden bg-grey-lighter">
-                <h2 class="accordion__header pt-4 pl-4">Header</h2>
-                <p class="accordion__body p-4" id="panel1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto possimus at a cum saepe molestias modi illo facere ducimus voluptatibus praesentium deleniti fugiat ab error quia sit perspiciatis velit necessitatibus.Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Lorem ipsum dolor sit amet.</p>
-            </div>
-        </div>
-        <!-- Panel 2 -->
-        <div class="w-1/2">
-            <input type="checkbox" name="panel" id="panel-2" class="hidden">
-            <label for="panel-2" class="relative block bg-black text-white p-4 shadow border-b border-grey">Panel 2</label>
-            <div class="accordion__content overflow-hidden bg-grey-lighter">
-                <h2 class="accordion__header pt-4 pl-4">Header</h2>
-                <p class="accordion__body p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto possimus at a cum saepe molestias modi illo facere ducimus voluptatibus praesentium deleniti fugiat ab error quia sit perspiciatis velit necessitatibus.Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Lorem ipsum dolor sit amet.</p>
-            </div>
-        </div>
-        <!--  Panel 3  -->
-        <div class="w-1/2">
-            <input type="checkbox" name="panel" id="panel-3" class="hidden">
-            <label for="panel-3" class="relative block bg-black text-white p-4 shadow border-b border-grey">Panel 3</label>
-            <div class="accordion__content overflow-hidden bg-grey-lighter">
-                <h2 class="accordion__header pt-4 pl-4">Header</h2>
-                <p class="accordion__body p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto possimus at a cum saepe molestias modi illo facere ducimus voluptatibus praesentium deleniti fugiat ab error quia sit perspiciatis velit necessitatibus.Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Lorem ipsum dolor sit amet.</p>
-            </div>
-        </div>
-    </div>
-
 @endsection
 
