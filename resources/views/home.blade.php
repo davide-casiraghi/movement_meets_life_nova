068b7ff569d149bf07b8a18e9ca671805df63f10
Davide Casiraghi <davide.casiraghi@agiledrop.com>
2020-10-25 21:00:02 +0100

    Adds video in jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,16 @@
 
 @extends('layouts.app')
 
-@section('content')
+@section('jumbotron')
     @include('partials.home.video_embed')
+@endsection
+
+@section('content')
 
     <h1 class="text-4xl font-bold text-red-500 text-center">Hello word!</h1>
     <img src="http://lorempixel.com/g/800/600/" alt="">
 
     @include('partials.home.blog')
-
-
     @include('partials.quote_of_the_day')
-
 
 @endsection
