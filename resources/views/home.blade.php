de73742f39ac58b09d561ce5d29ae29888f4de14
Davide Casiraghi <davide.casiraghi@agiledrop.com>
2020-11-07 11:57:39 +0100

    Add ILM block to HP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 @extends('layouts.app')
 
 @section('jumbotron')
-    @include('partials.home.video_embed')
+    {{--@include('partials.home.video_embed')--}}
+
 @endsection
 
 @section('content')
 
-    <h1 class="text-4xl font-bold text-red-500 text-center">Hello word!</h1>
-    <img src="http://lorempixel.com/g/800/600/" alt="">
+    @include('partials.pages.home.ilm')
 
     @include('partials.home.blog')
     @include('partials.quote_of_the_day')
