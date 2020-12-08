f1046e59eff378514a12e48f0c7fa722799e41f2
Davide Casiraghi <davide.casiraghi@gmail.com>
2020-12-08 16:36:12 +0100

    Removes Tailwind ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -155,7 +155,7 @@ module.exports = {
     },
 
     plugins: [
-        require('@tailwindcss/ui'),
+        //require('@tailwindcss/ui'),
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
