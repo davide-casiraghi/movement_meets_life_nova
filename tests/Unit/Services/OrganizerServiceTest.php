520a6de8a07fc9b70e97791560617991f009ecc0
Davide Casiraghi <davide.casiraghi@gmail.com>
2020-12-25 09:37:54 +0100

    Add test it_should_update_an_organizer

--- tests/Unit/Services/OrganizerServiceTest.php
+++ tests/Unit/Services/OrganizerServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Services;
 
 use App\Http\Requests\OrganizerStoreRequest;
+use App\Models\Organizer;
 use App\Models\User;
 use App\Services\OrganizerService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -15,6 +16,9 @@ class OrganizerServiceTest extends TestCase{
     use RefreshDatabase; // empty the test DB
 
     private OrganizerService $organizerService;
+
+    private User $user1;
+    private Organizer $organizer1;
 
     /**
      * Populate test DB with dummy data.
@@ -35,6 +39,8 @@ class OrganizerServiceTest extends TestCase{
             'name' => 'Paolo',
             'email' => 'admin@gmail.com',
         ]);
+
+        $this->organizer1 = Organizer::factory()->create();
     }
 
     /** @test */
@@ -45,7 +51,6 @@ class OrganizerServiceTest extends TestCase{
         $request = new OrganizerStoreRequest();
 
         $data = [
-            'user_id' => '1',
             'name' => 'test new name',
             'surname' => 'test surname',
             'email' => 'test@newemail.com',
@@ -59,5 +64,26 @@ class OrganizerServiceTest extends TestCase{
         $this->organizerService->createOrganizer($request);
 
         $this->assertDatabaseHas('organizers', ['name' => 'test new name']);
+    }
+
+    /** @test */
+    public function it_should_update_an_organizer()
+    {
+        $request = new OrganizerStoreRequest();
+
+        $data = [
+            'name' => 'name updated',
+            'surname' => 'test surname',
+            'email' => 'test@newemail.com',
+            'description' => 'test@newemail.com',
+            'website' => 'test@newemail.com',
+            'facebook' => 'test@newemail.com',
+            'phone' => 'test@newemail.com',
+        ];
+        $request->merge($data);
+
+        $this->organizerService->updateOrganizer($request, $this->organizer1->id);
+
+        $this->assertDatabaseHas('organizers', ['name' => 'name updated']);
     }
 }
