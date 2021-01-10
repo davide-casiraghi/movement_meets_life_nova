56293d97e65a12267e80ca7f8f1880724af376f6
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-01-10 18:50:43 +0100

    Install Select 2

---
+++ resources/js/vendors/select2.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    /**
+     * Add Select2 multiple selection to all the divs with class .select2-multiple
+     */
+    $(".select2-multiple").select2({
+        placeholder: "Select one or more",
+        width: '100%'
+    });
+
+});
