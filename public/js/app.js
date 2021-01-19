10723c4e6b9dde0839cac53a8f08a52d828dd3be
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-01-19 07:41:50 +0100

    Add Datepicker for past and future

--- public/js/app.js
+++ public/js/app.js
@@ -118585,15 +118585,31 @@ $(document).ready(function () {
 /***/ (function(module, exports) {
 
 $(document).ready(function () {
-  var today = new Date(); // Normal datepicker - Enabled just from today (no past allowed)
+  var today = new Date(); // Single selection - Past and future allowed
 
-  $('.datepicker input').datepicker({
+  $('.datepicker.all input').datepicker({
+    format: 'dd/mm/yyyy',
+    daysOfWeekHighlighted: "6,0",
+    weekStart: 1,
+    todayHighlight: true
+  }); // Single selection - Just for past dates
+
+  $('.datepicker.past input').datepicker({
+    format: 'dd/mm/yyyy',
+    daysOfWeekHighlighted: "6,0",
+    weekStart: 1,
+    endDate: today,
+    todayHighlight: true
+  }); // Single selection -  Just for future dates
+
+  $('.datepicker.past input').datepicker({
     format: 'dd/mm/yyyy',
     daysOfWeekHighlighted: "6,0",
     weekStart: 1,
     startDate: today,
     todayHighlight: true
-  });
+  }); // Multiple date selection
+
   $('.datepickerMultiple input').datepicker({
     format: 'dd/mm/yyyy',
     daysOfWeekHighlighted: "6,0",
--- resources/js/vendors/bootstrap-datepicker.js
+++ resources/js/vendors/bootstrap-datepicker.js
@@ -2,16 +2,33 @@ $(document).ready(function() {
 
     var today = new Date();
 
-    // Normal datepicker - Enabled just from today (no past allowed)
-    $('.datepicker input').datepicker({
+    // Single selection - Past and future allowed
+    $('.datepicker.all input').datepicker({
+        format: 'dd/mm/yyyy',
+        daysOfWeekHighlighted: "6,0",
+        weekStart: 1,
+        todayHighlight: true,
+    });
+
+    // Single selection - Just for past dates
+    $('.datepicker.past input').datepicker({
+        format: 'dd/mm/yyyy',
+        daysOfWeekHighlighted: "6,0",
+        weekStart: 1,
+        endDate : today,
+        todayHighlight: true,
+    });
+
+    // Single selection -  Just for future dates
+    $('.datepicker.past input').datepicker({
         format: 'dd/mm/yyyy',
         daysOfWeekHighlighted: "6,0",
         weekStart: 1,
         startDate: today,
         todayHighlight: true,
     });
-
-
+    
+    // Multiple date selection
     $('.datepickerMultiple input').datepicker({
         format: 'dd/mm/yyyy',
         daysOfWeekHighlighted: "6,0",
