09d91d628fa64924eae0aa2d2f9385fe688ea3ad
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-01-23 09:43:31 +0100

    Align right country, teams and user level in user index view

--- resources/views/partials/users/indexItem.blade.php
+++ resources/views/partials/users/indexItem.blade.php
@@ -12,7 +12,7 @@
                 </div>
             </div>
             <div class="mt-2 flex sm:justify-between">
-                <div class="flex sm:justify-start text-sm text-gray-500 sm:mt-0">
+                <div class="sm:flex sm:justify-start text-sm text-gray-500 sm:mt-0">
                     {{-- Country --}}
                     <div class="flex">
                         <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clip-rule="evenodd"></path></svg>
@@ -21,7 +21,7 @@
 
                     {{-- User level --}}
                     @if($user->isAdmin())
-                        <div class="flex ml-2">
+                        <div class="flex sm:ml-2">
                             <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path></svg>
                             <p>{{$user->level}}</p>
                         </div>
@@ -29,7 +29,7 @@
 
                     {{-- Team membership --}}
                     @if($user->isAdmin() && $user->teams->count() > 0)
-                        <div class="flex ml-2">
+                        <div class="flex sm:ml-2">
                             <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13 7H7v6h6V7z"></path><path fill-rule="evenodd" d="M7 2a1 1 0 012 0v1h2V2a1 1 0 112 0v1h2a2 2 0 012 2v2h1a1 1 0 110 2h-1v2h1a1 1 0 110 2h-1v2a2 2 0 01-2 2h-2v1a1 1 0 11-2 0v-1H9v1a1 1 0 11-2 0v-1H5a2 2 0 01-2-2v-2H2a1 1 0 110-2h1V9H2a1 1 0 010-2h1V5a2 2 0 012-2h2V2zM5 5h10v10H5V5z" clip-rule="evenodd"></path></svg>
                             {{$user->teams->implode(', ')}}
                         </div>
