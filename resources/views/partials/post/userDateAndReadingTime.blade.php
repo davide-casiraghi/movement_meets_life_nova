0a0729907c56e3c844edd14cffc373bc5d282be2
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-01-26 14:07:49 +0100

    Fix post title, by and date with correct Tailwindcss classes

--- resources/views/partials/post/userDateAndReadingTime.blade.php
+++ resources/views/partials/post/userDateAndReadingTime.blade.php
@@ -1,8 +1,8 @@
-<div class="grid grid-flow-col auto-cols-max md:auto-cols-min text-base {{$textColor}}">
-    <div class="pr-10">
-        By {{$post->user->name}}
+<div class="md:grid md:grid-cols-6 md:gap-6 text-base {{$textColor}}">
+    <div class="md:col-span-3">
+        By {{$post->user->profile->full_name}}
     </div>
-    <div class="">
+    <div class="md:mt-0 md:col-span-3">
         <div class="float-right">
             {{ $post->created_at->format('M j, Y') }} |
             <div class="inline-block">
