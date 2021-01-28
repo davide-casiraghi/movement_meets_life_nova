f37599f9a63436d74e07472c2a3938573f226c9b
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-01-28 19:56:00 +0100

    Add quote index

---
+++ resources/views/quotes/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.backend')
+
+@section('title')
+    @lang('views.tag_management')
+@endsection
+
+@section('buttons')
+
+    @include('partials.forms.button',[
+        'title' => 'Add tag',
+        'url' => route('tags.create'),
+        'color' => 'indigo',
+        'icon' => '',
+        'size' => 1,
+        'extraClasses' => 'mb-4',
+        'kind' => 'primary',
+        'target' => '_self',
+    ])
+
+@endsection
+
+@section('content')
+
+    {{--@include('partials.quotes.searchBar')--}}
+
+    {{-- Tailwind Component: https://tailwindui.com/components/application-ui/lists/stacked-lists--}}
+    <div class="bg-white shadow overflow-hidden sm:rounded-md">
+        <ul class="divide-y divide-gray-200">
+            @foreach($quotes as $quote)
+                @include('partials.quotes.indexItem', [
+                    'quote' => $quote
+                ])
+            @endforeach
+        </ul>
+    </div>
+
+    <div class="my-5">
+        {{ $quotes->links() }}
+    </div>
+
+
+@endsection
