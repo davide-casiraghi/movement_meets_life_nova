0d9757338c0763298ad5499328aef86b1d45cb6a
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-01-28 15:01:04 +0100

    Clean code in backend layout

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -1,12 +1,13 @@
 @php ($barsBackground = '#B5A575')
 
-        <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="{{$barsBackground}}" />
+
     {{-- Theming the browser's address bar to match your brand's colors provides a more immersive user experience.--}}
     <meta name="description" content="@hasSection('description')@yield('description')@else @lang('homepage-serach.find_information')@endif" />
 
@@ -19,10 +20,12 @@
     {{-- CSS --}}
     <link href="{{ asset('css/vendor.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
-    @yield('css') {{-- JS that need to stay in the head--}} @yield('javascript-head') {{--
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" />
-        --}} @livewireStyles
+    @yield('css')
+
+    {{-- JS that need to stay in the head--}}
+    @yield('javascript-head')
+
+    @livewireStyles
 </head>
 
 <body class="bg-gray-100">
@@ -52,9 +55,7 @@
                     </div>
                 </div>
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 mt-4">
-                    <!-- Replace with your content -->
-                @yield('content')
-                <!-- /End replace -->
+                    @yield('content')
                 </div>
             </div>
         </main>
