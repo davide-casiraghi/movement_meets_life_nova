493307e4a1f8b8db5c5a94330e8de6b375ce853a
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-01-29 12:46:23 +0100

    Fix globalSearch layout

--- resources/views/globalSearch/index.blade.php
+++ resources/views/globalSearch/index.blade.php
@@ -6,23 +6,23 @@
 
 @section('content')
 
-    <div>
+    <div class="mb-3">
         <b>{{ $searchResults->count() }} results found for "{{ request('keyword') }}"</b>
     </div>
 
-
-    <div class="bg-white shadow overflow-hidden sm:rounded-md">
-        <ul class="divide-y divide-gray-200">
-            @foreach($searchResults->groupByType() as $type => $modelSearchResults)
-
-                <h2>{{ ucfirst($type) }}</h2>
-
-                @foreach($modelSearchResults as $searchResult)
-                    @include('partials.globalSearch.indexItem', [
-                        'searchResult' => $searchResult
-                    ])
-                @endforeach
-            @endforeach
-        </ul>
+    <div class="mt-5">
+        @foreach($searchResults->groupByType() as $type => $modelSearchResults)
+            <h2 class="mb-2">{{ ucfirst($type) }}</h2>
+            <div class="bg-white shadow overflow-hidden sm:rounded-md mb-5">
+                <ul class="divide-y divide-gray-200">
+                    @foreach($modelSearchResults as $searchResult)
+                        @include('partials.globalSearch.indexItem', [
+                            'searchResult' => $searchResult
+                        ])
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
     </div>
+
 @endsection
