292fdb3d1bc2457621635b7d71c84d1b5b61e645
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-03 17:25:30 +0100

    Save gallery images title and description with Livewire

--- app/Http/Livewire/GalleryImagesParameters.php
+++ app/Http/Livewire/GalleryImagesParameters.php
@@ -4,10 +4,22 @@ namespace App\Http\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class GalleryImagesParameters extends Component
 {
     public $model;
+    public $image;
+
+    public $title;
+    public $description;
+
+    public $showModal = false;
+
+    protected $rules = [
+        'title' => 'string',
+        'description' => 'string',
+    ];
 
     public function mount($model)
     {
@@ -17,5 +29,28 @@ class GalleryImagesParameters extends Component
     public function render()
     {
         return view('livewire.gallery-images-parameters');
+    }
+
+    public function edit($imageId)
+    {
+        $this->showModal = true;
+        $this->image = Media::find($imageId);
+
+        $this->title = $this->image->getCustomProperty('title');
+        $this->description = $this->image->getCustomProperty('description');
+    }
+
+    public function save()
+    {
+        $this->image->setCustomProperty('title', $this->title);
+        $this->image->setCustomProperty('description', $this->description);
+        $this->image->save();
+        
+        $this->showModal = false;
+    }
+
+    public function close()
+    {
+        $this->showModal = false;
     }
 }
