1ceaba01368a3229a1ae8dd38079dca33d49de45
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-04 16:20:32 +0100

    Retrieve the gallery images and show the Fancybox, also with videos

--- app/Services/Snippets/GalleryMasonryService.php
+++ app/Services/Snippets/GalleryMasonryService.php
@@ -18,25 +18,28 @@ namespace App\Services\Snippets;
         // Options: http://miromannino.github.io/Justified-Gallery/options-and-events/
 */
 
+use App\Models\Post;
+
 class GalleryMasonryService
 {
 
     /**
      *  Substitute gallery snippets with the related HTML
      *
-     * @param $postBody
+     * @param \App\Models\Post $post
      *
-     * @return array $ret             the extension
+     * @return string
      */
-    public function snippetsToHTML($postBody)
+    public function snippetsToHTML(Post $post): string
     {
         $storagePath = storage_path('app/public');
         $publicPath = public_path();
 
         // Find plugin occurrences
-        $ptn = '/{# +gallery +(src|width|height)=\[(.*)\] +(src|width|height)=\[(.*)\] +(src|width|height)=\[(.*)\] +#}/';
+        //$ptn = '/{# +gallery +(name|width)=\[(.*)\] +(name|width)=\[(.*)\] +(name|width)=\[(.*)\] +#}/';
+        $ptn = '/{# +gallery +(name|width)=\[(.*)\] +(name|width)=\[(.*)\] +#}/';
 
-        if (preg_match_all($ptn, $postBody, $matches)) {
+        if (preg_match_all($ptn, $post->body, $matches)) {
             // Trasform the matches array in a way that can be used
             $matches = $this->turn_array($matches);
 
@@ -44,7 +47,24 @@ class GalleryMasonryService
                 // Get plugin parameters array
                 $parameters = $this->getParameters($single_gallery_matches, $storagePath, $publicPath);
 
-                if (is_dir($parameters['images_dir'])) {
+                ray($parameters);
+
+                if (self::postHasGallery($post, $parameters['gallery_name'])) {
+                    $galleryHtml = 'ciao1';
+
+                    $images = $this->createImagesArray($post, $parameters['gallery_name']);
+
+                    // Prepare Gallery HTML
+                    $galleryHtml = $this->prepareGallery($images);
+
+
+
+                } else {
+                    $galleryHtml = "<div class='alert alert-warning' role='alert'>A gallery with this name not available for this element</div>";
+                }
+
+
+                /*if (is_dir($parameters['images_dir'])) {
                     // Get images file name array
                     $image_files = $this->getImageFiles($parameters['images_dir']);
                     //sort($image_files,SORT_STRING);
@@ -65,11 +85,31 @@ class GalleryMasonryService
                         $galleryHtml = "<div class='alert alert-warning' role='alert'>The directory specified exist but it doesn't contain images</div>";
                     }
                 } else {
-                    $galleryHtml = "<div class='alert alert-warning' role='alert'>Image directory not found</div>";
-                }
+                    $galleryHtml = "<div class='alert alert-warning' role='alert'>A gallery with this name not available for this element</div>";
+                }*/
+
+
+
+
+                //$galleryHtml = 'ciao';
+
+
+
+
+
+
+
+
+                $galleryHtml .= "</div>";
+
+
+
+
+
+
 
                 // Replace the TOKEN found in the article with the generatd gallery HTML
-                $postBody = str_replace($parameters['token'], $galleryHtml, $postBody);
+                $postBody = str_replace($parameters['token'], $galleryHtml, $post->body);
             }
         }
 
@@ -84,143 +124,94 @@ class GalleryMasonryService
     public function getParameters($matches, $storagePath, $publicPath)
     {
         $ret = [];
-
-        // Get Paths
-        $sitePath = '/';
-        $siteUrl = $publicPath;
+        //ray($matches);
 
         // Get activation string parameters (from article)
         $ret['token'] = $matches[0];
-        $subDir = $matches[2];
-        $ret['photo_width'] = $matches[4];
-        $ret['photo_height'] = $matches[6];
 
-        // Directories
-        $ret['images_dir'] = $publicPath.'/images/'.$subDir.'/';
-        $ret['thumbs_dir'] = $publicPath.'/images/'.$subDir.'/thumb/';
-
-        // Thumbnails size
-        $ret['thumbs_size']['width'] = 300;
-        $ret['thumbs_size']['height'] = 300;
-
-        // URL variables
-        $ret['gallery_url'] = '/images/'.$subDir.'/';
-        $ret['thumb_url'] = '/images/'.$subDir.'/thumbs/';
+        $ret['gallery_name'] = $matches[2];
+        $ret['thumbnail_width'] = $matches[4];
 
         return $ret;
     }
 
-    // **********************************************************************
-
     /**
-     *  Generate a single thumbnail file from an image.
-     *  @param array $src               path of the original image
-     *  @param array $dest              path of the generated thumbnail
-     *  @param array $desired_width     width of the thumbnail
+     *  Returns true if the post has the specified gallery.
      *
-     *  @return void
-     **/
-    public function generate_single_thumb_file($src, $dest, $desired_width, $desired_height): void
+     * @param \App\Models\Post $post
+     * @param string $galleryName
+     *
+     * @return bool $ret
+     */
+    public function postHasGallery(Post $post, string $galleryName): bool
     {
-        // Read the source image
-        $source_image = imagecreatefromjpeg($src);
-
-        // Get width and height of the original image
-        $width = imagesx($source_image);
-        $height = imagesy($source_image);
-
-        // find the "desired height" of this thumbnail, relative to the desired width
-        //$desired_height = floor($height*($desired_width/$width));
-
-        // now thumbnails are square so is the same of width
-        //$desired_height = $desired_width;
-
-        // Horizontal image
-        if ($width > $height) {
-            $desired_width = 400;
-            $desired_height = floor($height * ($desired_width / $width));
+        if (in_array($galleryName, self::getGalleryNames($post))) {
+            return true;
         }
-        // Vertical image
-        else {
-            $desired_height = 500;
-            $desired_width = floor($width * ($desired_height / $height));
-        }
-
-        // Create a new, "virtual" image
-        $virtual_image = imagecreatetruecolor($desired_width, $desired_height);
-
-        // Copy source image at a resized size
-        imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
-
-        //Create the physical thumbnail image to its destination
-        imagejpeg($virtual_image, $dest);
+        return false;
     }
 
-    // **********************************************************************
+    /**
+     * Return the array with the gallery names.
+     * The gallery names are set as an image property.
+     *
+     * @param \App\Models\Post $post
+     *
+     * @return array
+     */
+    public function getGalleryNames(Post $post): array
+    {
+        $galleries = [];
+        foreach ($post->getMedia('images') as $image) {
+            $galleries[$image->getCustomProperty('image_gallery')] = $image->getCustomProperty('image_gallery');
+        }
+        return array_values($galleries);
+    }
 
     /**
-     *  Generate all the thumbnails of the gallery.
-     *  @param string $images_dir        images dir on the server
-     *  @param string $thumbs_dir        thumb dir on the server
-     *  @param array $image_files
-     *  @return void
-     **/
-    public function generateThumbs($images_dir, $thumbs_dir, $thumbs_size, $image_files): void
+     * Return the array with the gallery images.
+     *
+     * @param \App\Models\Post $post
+     * @param string $galleryName
+     *
+     * @return array
+     */
+    public function getGalleryImages(Post $post, string $galleryName): array
     {
-
-        // Thumbnails size
-        $thumbs_width = $thumbs_size['width'];
-        $thumbs_height = $thumbs_size['height'];
-
-        //  Create thumbs dir
-        if (! is_dir($thumbs_dir)) {
-            mkdir($thumbs_dir);
-        }
-
-        // Generate missing thumbs
-        if (count($image_files)) {
-            $index = 0;
-            foreach ($image_files as $index=>$file) {
-                $index++;
-                $thumbnail_image = $thumbs_dir.$file;
-                if (! file_exists($thumbnail_image)) {
-                    $extension = $this->get_file_extension($thumbnail_image);
-                    if ($extension) {
-                        //echo $images_dir." ".$file." ".$thumbnail_image." ".$thumbs_width;
-                        $this->generate_single_thumb_file($images_dir.$file, $thumbnail_image, $thumbs_width, $thumbs_height);
-                    }
-                }
+        $galleryImages = [];
+        foreach ($post->getMedia('images') as $image) {
+            if ($image->getCustomProperty('image_gallery') == $galleryName) {
+                $galleryImages[] = $image;
             }
         }
+        return $galleryImages;
     }
 
 
     /**
      *  Create images array.
      *
-     * @param array $image_files array with all the image names
-     * @param $image_data
-     * @param $gallery_url
+     * @param \App\Models\Post $post
+     * @param string $galleryName
      *
      * @return array $ret    array with the images datas
      */
-    public function createImagesArray($image_files, $image_data, $gallery_url)
+    public function createImagesArray(Post $post, string $galleryName): array
     {
-
-        //dump($image_data, "image data");
+        $images = self::getGalleryImages($post, $galleryName);
 
         // Order by image name
-        sort($image_files);
+        //sort($image_files);
 
         $ret = [];
 
-        foreach ($image_files as $k => $image_file) {
-            $ret[$k]['file_path'] = $gallery_url.$image_file;
-            $ret[$k]['thumb_path'] = $gallery_url.'thumb/'.$image_file;
-            $ret[$k]['description'] = $image_data[$image_file]['description'];
-            $ret[$k]['video_link'] = $image_data[$image_file]['video'];
+        foreach ($images as $k => $image) {
+            $ret[$k]['file_path'] = $image->getUrl();
+            $ret[$k]['thumb_path'] = $image->getUrl('thumb');
+
+            $ret[$k]['description'] = $image->getCustomProperty('image_description');
+            $ret[$k]['video_link'] = $image->getCustomProperty('image_video_url');
         }
-        //dump($ret,"images");
 
         return $ret;
     }
@@ -240,16 +231,13 @@ class GalleryMasonryService
 
     /**
      *  Prepare the gallery HTML.
-     *  @param array $images                        Images array [file_path, short_desc, long_desc]
-     *  @param array $bootstrapDeviceImageWidth     array that contain the sizes of the images
-     *                                              for the four kind of bootrap devices classes ()
-     *                                              xs (phones), sm (tablets), md (desktops), and lg (larger desktops)
-     *  @param ****array $desired_width     width of the thumbnail
-     *  @return string $ret             the HTML to print on screen
-     **/
-    public function prepareGallery($images)
+     *
+     * @param array $images Images array [file_path, short_desc, long_desc]
+     *
+     * @return string $ret             the HTML to print on screen
+     */
+    public function prepareGallery(array $images)
     {
-
         // Animate item on hover
         $itemClass = 'animated';
 
@@ -257,14 +245,13 @@ class GalleryMasonryService
         $ret = "<div class='gallery'>";
 
         foreach ($images as $k => $image) {
-
             // Get item link
             $imageLink = ($image['video_link'] == null) ? $image['file_path'] : $image['video_link'];
             $videoPlayIcon = ($image['video_link'] == null) ? '' : "<i class='far fa-play-circle'></i>";
 
-            $ret .= "<div class='item ".$itemClass."'>";
-            $ret .= "<a href='".$imageLink."' data-fancybox='images' data-caption='".$image['description']."'>";
-            $ret .= "<img src='".asset($image['thumb_path'])."' />";
+            $ret .= "<div class='item " . $itemClass . "'>";
+            $ret .= "<a href='" . $imageLink . "' data-fancybox='images' data-caption='" . $image['description'] . "'>";
+            $ret .= "<img src='" . asset($image['thumb_path']) . "' />";
             $ret .= $videoPlayIcon;
             $ret .= '</a>';
             $ret .= '</div>';
