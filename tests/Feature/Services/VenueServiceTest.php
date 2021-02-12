5870278f8f7c3c557863d5ad83d8fc9816296428
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-12 10:41:11 +0100

    Test itShouldGetVenueGpsCoordinates() in VenueService

--- tests/Feature/Services/VenueServiceTest.php
+++ tests/Feature/Services/VenueServiceTest.php
@@ -9,6 +9,7 @@ use App\Services\VenueService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use VCR\VCR;
 
 class VenueServiceTest extends TestCase
 {
@@ -115,4 +116,26 @@ class VenueServiceTest extends TestCase
         $numberVenuesCreatedLastThirtyDays = $this->venueService->getNumberVenuesCreatedLastThirtyDays();
         $this->assertEquals($numberVenuesCreatedLastThirtyDays, 3);
     }
+
+    /** @test */
+    public function itShouldGetVenueGpsCoordinates()
+    {
+        // Tutorial for this test: https://laracasts.com/lessons/testing-http-requests
+        // the output of the API request is saved in the tests/fixtures directory.
+        // So after the first time, it reads this file instead of doing other requests.
+
+        $address = "Milano, viale Abruzzi, 2";
+
+        VCR::turnOn();
+        VCR::insertCassette('mapquestapi');
+        $coordinates = $this->venueService->getVenueGpsCoordinates($address);
+
+        VCR::eject();
+        VCR::turnOff();
+
+        $this->assertEquals(45, number_format($coordinates['lat'], 0));
+        $this->assertEquals(9, number_format($coordinates['lng'], 0));
+    }
+
+
 }
