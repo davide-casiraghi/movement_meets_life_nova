9c7a5b39b962c3ec939f45332e0b741a439b3c2a
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-13 18:02:24 +0100

    Fix blog padding x for mobile and pagination language file

--- resources/views/posts/blog.blade.php
+++ resources/views/posts/blog.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-    <div class="max-w-4xl mx-auto">
+    <div class="max-w-4xl mx-auto px-8 sm:px-0">
 
-        <div class="border-gray-400 border-solid border-0 box-border leading-6 pt-6 pb-8 text-black" style="quotes: auto;">
-            <h1 class="sm:text-4xl md:text-6xl border-solid box-border font-extrabold text-3xl m-0 text-gray-900 tracking-tight"  style="line-height: 1.2; quotes: auto;">
+        <div class="border-gray-400 border-solid border-0 box-border leading-6 pt-6 pb-8 text-black">
+            <h1 class="sm:text-4xl md:text-6xl border-solid box-border font-extrabold text-3xl m-0 text-gray-900 tracking-tight mb-2">
                 Latest
             </h1>
-            <p class="border-solid box-border text-lg m-0 text-gray-600"  style="line-height: 1.55556; --space-y-reverse:0; quotes: auto;">
+            <p class="border-solid box-border text-lg m-0 text-gray-500">
                 All the latest news from the blog.
             </p>
         </div>
