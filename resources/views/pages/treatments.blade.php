414c69fd8d4373bd6c67997339bd010635e159b1
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-14 21:07:34 +0100

    Add contents to treatments

--- resources/views/pages/treatments.blade.php
+++ resources/views/pages/treatments.blade.php
@@ -13,7 +13,84 @@
             </p>
         </div>
 
-        treatmetns details
+        <div class="">
+            The Ilan Lev Method (ILM) is a revolutionary form of hands-on bodywork that addresses body aches, pains, and movement imbalances caused by physical or emotional strain.
+        </div>
+        
+        <div class="">
+            In April 2019 I've completed the first Ilan Lev Method training program in Europe and I'm a certified practitioner.
+            I give private treatments in Ljubljana, Slovenia.
+        </div>
+        
+        <div class="">
+            Ilan Lev Method Bodywork
+        </div>
+        <div class="">
+            During a session, the practitioner brings to the body of the receiver a wide variety of movements, vibrations, playful provocations. These create the environment in which the body's natural ability to heal itself can arise, we discover an optimal range of motion, efficiency of effort, and a new understanding of ourselves.
+            This allows new physical pathways and coordinations to emerge or become clearer, old habits to be thrown out, and physical and emotional trauma to be released.
+            In this way, the movement returns to parts of the body where communication was cut-off or stopped due to injury, pain, or emotional issues. And the dialogue within the whole system is restored.
+        </div>
+        
+        <div class="">
+            "I find Ilan's work an amazing gift to dancers and people alike. His work helps the body's own regeneration. It brings movement to blocked areas in the body, it comes with positive energy and a sense of joy, it can help quicker recovery from "fresh injuries" as well as heal ״old״ ones... It helped me to become more efficient with my movement and gave me a more balanced body... His work is intelligent, thoughtful, considerate, and serves as a constant research method for growth, for Ilan himself as well as for those he works with."
+- Maestro Ohad Naharin, Choreographer -Artistic Director of Batsheva Dance Company - 
+
+"The doctors in San Diego have recommended that I go to an urgent operation...after surviving 20-hours of a horrible long-distance flight, from the West Coast to Tel-Aviv, I met my old friend Ilan Lev. Within two-three weeks I went back to conduct the symphonic orchestra in San Diego... To the great surprise of the hospital doctors there...They could not believe the 'miracle' that has just occurred."
+- Maestro Yoav Talmi, Conductor - Music Director of Orchestre Symphonique de Quebec, Canada -
+        </div>
+        
+        <div class="">
+            Movement classes according to the Ilan Lev Method
+
+The classes are made to improve the inner body dialogue and develop our functioning abilities out of the motion of inner listening and self pulsating.
+In these group classes, the participants are laying on the floor, sitting or moving. The class process is refreshing, intriguing and increasing the body's vitality. Through various images, we learn to break through functional patterns that we have collected over the years, with the understanding that this is how the deepest learning takes place. 
+
+The activity is intended for all those who want to improve their functioning abilities.
+        </div>
+        
+        <div class="">
+            More info
+Please enjoy this video about the method: Youtube >
+To keep updated on my latest articles and special offers you can follow my Facebook page 
+More about ILM >
+        </div>
+        
+        <div class="">
+            Extra resources
+If you are new to the somatic world I find very interesting and clear this description of somatic education provided by the Rolling Point association in Vienna.
+
+I think that it can be also helpful this quote:
+Somatic bodywork involves someone else's eyes and senses on your body, to help you find "lost" parts of yourself, areas subject to "sensori-motor amnesia" that only outside pair of hands can help you discover, because they have fallen out of your body image.
+- Tom Meyers -
+
+        </div>
+        
+        
+        <div class="">
+            Here is your treatment
+Contact: Davide Casiraghi, +38669627872, davide.casiraghi@gmail.com
+Treatment duration: 60-90 minutes per session
+By appointment
+        </div>
+        
+        <div class="">
+            What the treatment is about
+Release
+Releases physical and mental tensions
+
+Refine
+Refining perceptions and sensitivity
+
+Enhance
+Awaken and enhance the body's motor functions and activate the senses
+
+Remove
+Remove obstacles in movements and functional difficulties
+        </div>
+        
+        <div class="">
+            Testimonials
+        </div>
     </div>
 
 @endsection
