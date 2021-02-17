0a392f98fffb1c8fcd7314c25967da4ad2b5d6b4
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-17 12:30:50 +0100

    Add video play icon using Fontawesome SVG

--- app/Services/Snippets/GalleryMasonryService.php
+++ app/Services/Snippets/GalleryMasonryService.php
@@ -184,7 +184,7 @@ class GalleryMasonryService
         foreach ($images as $k => $image) {
             // Get item link
             $imageLink = ($image['video_link'] == null) ? $image['file_path'] : $image['video_link'];
-            $videoPlayIcon = ($image['video_link'] == null) ? '' : "<i class='far fa-play-circle absolute text-6xl text-white inset-center opacity-80'></i>";
+            $videoPlayIcon = ($image['video_link'] == null) ? '' : "<svg class='absolute w-14 fill-current text-white inset-center opacity-80' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M371.7 238l-176-107c-15.8-8.8-35.7 2.5-35.7 21v208c0 18.4 19.8 29.8 35.7 21l176-101c16.4-9.1 16.4-32.8 0-42zM504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256z'/></svg>";
 
             $ret .= "<div class='item " . $itemClass . "'>";
             $ret .= "<a href='" . $imageLink . "' data-fancybox='images' data-caption='" . $image['description'] . "'>";
--- app/Services/Snippets/ImageService.php
+++ app/Services/Snippets/ImageService.php
@@ -123,11 +123,10 @@ class ImageService
         $imagePath = $image->getUrl();
         $thumbnailPath = $image->getUrl('thumb');
         $videoUrl = $image->getCustomProperty('image_video_url');
-
-
-
+        
         $imageLink = ($videoUrl == null) ? $imagePath : $videoUrl;
-        $videoPlayIcon = ($videoUrl == null) ? '' : "<i class='far fa-play-circle absolute text-6xl text-white inset-center opacity-80'></i>";
+        //$videoPlayIcon = ($videoUrl == null) ? '' : "<i class='far fa-play-circle absolute text-6xl text-white inset-center opacity-80'></i>";
+        $videoPlayIcon = ($videoUrl == null) ? '' : "<svg class='absolute w-14 fill-current text-white inset-center opacity-80' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M371.7 238l-176-107c-15.8-8.8-35.7 2.5-35.7 21v208c0 18.4 19.8 29.8 35.7 21l176-101c16.4-9.1 16.4-32.8 0-42zM504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256z'/></svg>";
 
 
         switch ($parameters['alignment']) {
