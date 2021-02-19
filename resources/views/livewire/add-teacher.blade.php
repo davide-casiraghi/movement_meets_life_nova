96a00ea5b63e62dad2e024d68564014d2237d830
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-19 16:29:46 +0100

    Add 'Add teacher' button

--- resources/views/livewire/add-teacher.blade.php
+++ resources/views/livewire/add-teacher.blade.php
@@ -10,35 +10,44 @@
         'extraClasses' => '',
     ])--}}
 
+    <div class="md:grid md:grid-cols-6 md:gap-6">
+        <div class="md:col-span-4">
+            <label for="teacher_ids" class="block text-sm font-medium text-gray-700 inline">{{__('general.teachers')}}</label>
+
+            <select id="teacher_ids" name="teacher_ids[]" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" multiple='multiple'>
+                <option value="" class="text-gray-500">{{__('event.select_teachers')}}</option>
+
+                @if(!empty($teachers))
+                    @foreach ($teachers as $key => $teacher)
+                        @isset($selected)
+                            <option value="{{$teacher->id}}" {{ in_array($teacher->id, $selected) ? "selected":"" }}>{{$teacher->full_name}}</option>
+                        @else
+                            <option value="{{$teacher->id}}">{{$teacher->full_name}}</option>
+                        @endif
+                    @endforeach
+                @endif
+            </select>
+
+            @error('teacher_ids')
+            <span class="invalid-feedback text-red-500" role="alert">
+                <strong>{{ $errors->first('teacher_ids') }}</strong>
+            </span>
+            @enderror
+        </div>
+        <div class="md:col-span-2 relative">
+            <button wire:click.prevent="edit" type="button" class="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 absolute bottom-1 left-1">
+                Add teacher
+            </button>
+        </div>
+    </div>
 
     
-    <label for="teacher_ids" class="block text-sm font-medium text-gray-700 inline">{{__('general.teachers')}}</label>
 
-    <select id="teacher_ids" name="teacher_ids[]" {{--autocomplete="teacher_ids"--}} class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" multiple='multiple'>
-        <option value="" class="text-gray-500">{{__('event.select_teachers')}}</option>
-
-        @if(!empty($teachers))
-            @foreach ($teachers as $key => $teacher)
-                @isset($selected)
-                    <option value="{{$teacher->id}}" {{ in_array($teacher->id, $selected) ? "selected":"" }}>{{$teacher->full_name}}</option>
-                @else
-                    <option value="{{$teacher->id}}">{{$teacher->full_name}}</option>
-                @endif
-            @endforeach
-        @endif
-    </select>
-
-    @error('teacher_ids')
-    <span class="invalid-feedback text-red-500" role="alert">
-        <strong>{{ $errors->first('teacher_ids') }}</strong>
-    </span>
-    @enderror
 </div>
 
 
 @push('scripts')
     <script>
-
         $(document).ready(function () {
             //console.log('ciao bello');
 
@@ -51,6 +60,6 @@
                 @this.set('selected', data);
             });
         });
-        
+
     </script>
 @endpush
