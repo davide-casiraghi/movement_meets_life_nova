c4c4cfeb0f9f336759ba2913465645c56cecf8af
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-22 10:02:26 +0100

    Save teacher from the livewire add teacher modal

--- app/Http/Livewire/AddTeacher.php
+++ app/Http/Livewire/AddTeacher.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Teacher;
+use App\Repositories\TeacherRepository;
+use App\Services\CountryService;
+use App\Services\TeacherService;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\App;
 use Livewire\Component;
 
 class AddTeacher extends Component
@@ -9,9 +15,22 @@ class AddTeacher extends Component
     public $teachers;
     public $selected;
     public $showModal = false;
+    public $newTeacher;
+
+    /*protected $rules = [
+        'teachers' => ['array'],
+    ];*/
 
     protected $rules = [
-        'teachers' => ['array'],
+        'newTeacher.country_id' => ['required', 'string'],
+        'newTeacher.name' => ['required', 'string', 'max:255'],
+        'newTeacher.surname' => ['required', 'string', 'max:255'],
+        'newTeacher.bio' => ['required', 'string'],
+        'newTeacher.year_starting_practice' => ['required', 'integer','min:1972'],
+        'newTeacher.year_starting_teach' => ['required', 'integer','min:1972'],
+        'newTeacher.significant_teachers' => ['required', 'string'],
+        'newTeacher.facebook' => ['nullable', 'url'],
+        'newTeacher.website' => ['nullable', 'url'],
     ];
 
     public function mount($teachers, $selected)
@@ -22,11 +41,15 @@ class AddTeacher extends Component
 
     public function render()
     {
-        return view('livewire.add-teacher');
+        $countryService = App::make(CountryService::class);
+        $countries = $countryService->getCountries();
+
+        return view('livewire.add-teacher', ['countries' => $countries]);
     }
 
     public function openModal()
     {
+        $this->teacher = new Teacher();
         $this->showModal = true;
     }
 
@@ -35,8 +58,42 @@ class AddTeacher extends Component
         $this->showModal = false;
     }
 
-    public function save()
+    public function saveTeacher()
     {
+        $teacherRepository = App::make(TeacherRepository::class);
+
+        $this->validate();
+
+        //dd($this->newTeacher);
+
+        /*$data = [
+            'country_id' => $this->newTeacher.country_id,
+            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'bio' => ['required', 'string'],
+            'year_starting_practice' => ['required', 'integer','min:1972','before_or_equal:today'],
+            'year_starting_teach' => ['required', 'integer','min:1972','before_or_equal:today'],
+            'significant_teachers' => ['required', 'string'],
+            'facebook' => ['nullable', 'url'],
+            'website' => ['nullable', 'url'],
+        ];*/
+
+        $teacherRepository->store($this->newTeacher);
+
+        session()->flash('message', 'Teacher added successfully 😁');
+        $this->showModal = false;
+
+        //$this->user->save();
+        //$this->success = true;
+
+
+
+
+        //
+
+
+        //$this->newComment = '';
+        //$this->image      = '';
         //
     }
 
--- resources/views/livewire/add-teacher.blade.php
+++ resources/views/livewire/add-teacher.blade.php
@@ -50,68 +50,193 @@
                 To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             -->
             <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-                <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
-                    <button wire:click="close" type="button" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span class="sr-only">Close</span>
-                        <!-- Heroicon name: x -->
-                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-                <div class=""> {{-- sm:flex sm:items-start --}}
+                <form wire:submit.prevent="saveTeacher">
+                    <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                        <button wire:click="close" type="button" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span class="sr-only">Close</span>
+                            <!-- Heroicon name: x -->
+                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class=""> {{-- sm:flex sm:items-start --}}
 
-                    aaaaaa
-                    {{--<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                        <h3 class="text-lg leading-6 font-medium text-gray-900 mb-4" id="modal-headline">
-                            Add image parameters
-                        </h3>
+                        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900 mb-4" id="modal-headline">
+                                @lang('teacher.create_new_teacher')
+                            </h3>
 
-                        <div class="mt-2">
-                            <label for="image_description" class="block text-sm font-medium text-gray-700">Description</label>
-                            <div class="mt-1">
-                                <input type="text" wire:model="image_description" id="image_description" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="">
+                            <div class="mt-2">
+                                @include('partials.forms.input', [
+                                        'label' => __('general.name'),
+                                        'name' => 'newTeacher.name',
+                                        'placeholder' => '',
+                                        'value' => old('newTeacher.name'),
+                                        'required' => true,
+                                        'disabled' => false,
+                                        'livewireSupport' => true,
+                                ])
                             </div>
-                        </div>
 
-                        <div>
-                            <label for="image_video_url" class="block text-sm font-medium text-gray-700 mt-3">Video URL</label>
-                            <div class="mt-1">
-                                <input type="text" wire:model="image_video_url" id="image_video_url" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="">
+                            <div class="mt-2">
+                                @include('partials.forms.input', [
+                                        'label' => __('general.surname'),
+                                        'name' => 'newTeacher.surname',
+                                        'placeholder' => '',
+                                        'value' => old('newTeacher.surname'),
+                                        'required' => false,
+                                        'disabled' => false,
+                                        'livewireSupport' => true,
+                                ])
                             </div>
-                        </div>
 
-                        <div>
-                            <label for="image_caption" class="block text-sm font-medium text-gray-700 mt-3">Caption</label>
-                            <div class="mt-1">
-                                <input type="text" wire:model="image_caption" id="image_caption" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="">
+                            <div class="mt-2">
+                                @include('partials.forms.select', [
+                                    'label' => __('general.country'),
+                                    'name' => 'newTeacher.country_id',
+                                    'placeholder' => '',
+                                    'records' => $countries,
+                                    //'selected' => $teacher->country_id,
+                                    'required' => true,
+                                    'extraClasses' => 'select2',
+                                    'livewireSupport' => true,
+                                ])
                             </div>
-                        </div>
 
-                        <div class="mt-2">
-                            <label for="image_gallery" class="block text-sm font-medium text-gray-700 mt-3">Gallery</label>
-                            <div class="mt-1">
-                                <input type="text" wire:model="image_gallery" id="image_gallery" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Name of the gallery to assign the image to">
+                            <div class="mt-2">
+                                @include('partials.forms.textarea', [
+                                       'label' => __('teacher.bio'),
+                                       'name' => 'newTeacher.bio',
+                                       'placeholder' => '',
+                                       'value' => old('newTeacher.bio'),
+                                       'required' => false,
+                                       'disabled' => false,
+                                       'style' => 'tinymce',
+                                       'extraDescription' => 'Anything to show jumbo style after the content',
+                                       'livewireSupport' => true,
+                                   ])
                             </div>
-                        </div>
 
-                        <div class="mt-2">
-                            <label for="snippet" class="block text-sm font-medium text-gray-700 mt-3">Snippet</label>
-                            <div class="mt-1">
-                                <input type="text" wire:model="snippet" id="snippet" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="">
+                            <div class="mt-2">
+                                @include('partials.forms.input', [
+                                        'label' => __('teacher.year_of_starting_to_practice'),
+                                        'name' => 'newTeacher.year_starting_practice',
+                                        'placeholder' => '',
+                                        'value' => old('newTeacher.year_starting_practice'),
+                                        'required' => false,
+                                        'disabled' => false,
+                                        'livewireSupport' => true,
+                                ])
                             </div>
-                        </div>
 
-                    </div>--}}
-                </div>
-                <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                    <button wire:click="save" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
-                        Save
-                    </button>
-                    <button wire:click="close" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm">
-                        Cancel
-                    </button>
-                </div>
+                            <div class="mt-2">
+                                @include('partials.forms.input', [
+                                        'label' => __('teacher.year_of_starting_to_teach'),
+                                        'name' => 'newTeacher.year_starting_teach',
+                                        'placeholder' => '',
+                                        'value' => old('newTeacher.year_starting_teach'),
+                                        'required' => false,
+                                        'disabled' => false,
+                                        'livewireSupport' => true,
+                                ])
+                            </div>
+
+                            <div class="mt-2">
+                                @include('partials.forms.textarea', [
+                                        'label' => __('teacher.significant_teachers'),
+                                        'name' => 'newTeacher.significant_teachers',
+                                        'placeholder' => '',
+                                        'value' => old('newTeacher.significant_teachers'),
+                                        'required' => false,
+                                        'disabled' => false,
+                                        'style' => 'plain',
+                                        'extraDescription' => 'Anything to show jumbo style after the content',
+                                        'livewireSupport' => true,
+                                    ])
+                            </div>
+
+                            <div class="mt-2">
+                                @include('partials.forms.input', [
+                                        'label' => __('general.website'),
+                                        'name' => 'newTeacher.website',
+                                        'placeholder' => '',
+                                        'value' => old('newTeacher.website'),
+                                        'required' => false,
+                                        'disabled' => false,
+                                        'livewireSupport' => true,
+                                ])
+                            </div>
+
+                            <div class="mt-2">
+                                @include('partials.forms.input', [
+                                        'label' => __('teacher.facebook_profile'),
+                                        'name' => 'newTeacher.facebook',
+                                        'placeholder' => '',
+                                        'value' => old('newTeacher.facebook'),
+                                        'required' => false,
+                                        'disabled' => false,
+                                        'livewireSupport' => true,
+                                ])
+                            </div>
+
+                            <div class="mt-2">
+                                @include('partials.forms.uploadImage', [
+                                          'label' => __('teacher.upload_profile_picture'),
+                                          'name' => 'newTeacher.profile_picture',
+                                          'required' => false,
+                                          'collection' => 'newTeacher.profile_picture',
+                                          //'entity' => $teacher,
+                                          'livewireSupport' => true,
+                                      ])
+                            </div>
+
+                            {{--<div class="mt-2">
+                                <label for="image_description" class="block text-sm font-medium text-gray-700">Description</label>
+                                <div class="mt-1">
+                                    <input type="text" wire:model="image_description" id="image_description" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="">
+                                </div>
+                            </div>
+
+                            <div>
+                                <label for="image_video_url" class="block text-sm font-medium text-gray-700 mt-3">Video URL</label>
+                                <div class="mt-1">
+                                    <input type="text" wire:model="image_video_url" id="image_video_url" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="">
+                                </div>
+                            </div>
+
+                            <div>
+                                <label for="image_caption" class="block text-sm font-medium text-gray-700 mt-3">Caption</label>
+                                <div class="mt-1">
+                                    <input type="text" wire:model="image_caption" id="image_caption" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="">
+                                </div>
+                            </div>
+
+                            <div class="mt-2">
+                                <label for="image_gallery" class="block text-sm font-medium text-gray-700 mt-3">Gallery</label>
+                                <div class="mt-1">
+                                    <input type="text" wire:model="image_gallery" id="image_gallery" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Name of the gallery to assign the image to">
+                                </div>
+                            </div>
+
+                            <div class="mt-2">
+                                <label for="snippet" class="block text-sm font-medium text-gray-700 mt-3">Snippet</label>
+                                <div class="mt-1">
+                                    <input type="text" wire:model="snippet" id="snippet" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="">
+                                </div>
+                            </div>--}}
+
+                        </div>
+                    </div>
+                    <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                        <button type="submit" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
+                            Save
+                        </button>
+                        <button wire:click="close" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm">
+                            Cancel
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
