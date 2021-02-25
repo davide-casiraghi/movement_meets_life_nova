ec0b67553f61ffa216c4017618aa5ed993546dd8
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-25 12:59:21 +0100

    Restyle the tag show page

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
     <div class="text-lg max-w-prose mx-auto mb-6 mt-8 sm:mt-32 px-10">
-        <h1 class="text-gray-900 text-3xl">Tag: {{$tag->tag}}</h1>
+        <h1 class="text-gray-900 text-3xl"># {{$tag->tag}}</h1>
+        <div class="mb-12 text-lg text-gray-500">
+            All the articles tagged with #{{$tag->tag}}.
+        </div>
         <div class="">
             @foreach($posts as $post)
                 <div class="mb-6">
