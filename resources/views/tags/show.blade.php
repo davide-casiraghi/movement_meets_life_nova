97dce45329552b9c5989ed1d891a14c5942067f9
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-25 12:56:43 +0100

    Refactor tag show view

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <h2 class="text-gray-900 text-3xl mx-4">Tag: {{$tag->tag}}</h2>
-    <div>
-    @foreach($posts as $post)
-        <a href="{{ route('posts.show',$post->id) }}">{{$post->title}}</a>
-        <br>
-        {{$post->intro_text}}<br><br>
-    @endforeach
-
+    <div class="text-lg max-w-prose mx-auto mb-6 mt-8 sm:mt-32 px-10">
+        <h1 class="text-gray-900 text-3xl">Tag: {{$tag->tag}}</h1>
+        <div class="">
+            @foreach($posts as $post)
+                <div class="mb-6">
+                    <a class="textLink" href="{{ route('posts.show',$post->id) }}">{{$post->title}}</a>
+                    <div class="">
+                    {{$post->intro_text}}
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
 
