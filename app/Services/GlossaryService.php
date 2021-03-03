26181ee8e407872f1a312a13fff90c2a4b5d9d72
Enrico Colombo <enrico.colombo@gmail.com>
2021-03-03 15:36:24 +0100

    Add creation of one glossary variant on glossary term create

--- app/Services/GlossaryService.php
+++ app/Services/GlossaryService.php
@@ -9,6 +9,7 @@ use App\Models\Glossary;
 use App\Models\GlossaryVariant;
 use App\Repositories\GlossaryRepository;
 use App\Repositories\GlossaryVariantRepository;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 class GlossaryService
 {
@@ -39,9 +40,32 @@ class GlossaryService
     public function createGlossary(GlossaryStoreRequest $request): Glossary
     {
         $glossary = $this->glossaryRepository->store($request->all());
+
+        $this->createGlossaryVariant($glossary);
+
         ImageHelpers::storeImages($glossary, $request, 'introimage');
 
         return $glossary;
+    }
+
+    /**
+     * Create one glossary variant that has the same name of the Glossary term
+     *
+     * @param  Glossary  $glossary
+     * @return GlossaryVariant
+     */
+    public function createGlossaryVariant(Glossary $glossary): GlossaryVariant
+    {
+        $data = [
+            'lang' => [],
+            'glossary_id' => $glossary->id
+        ];
+        $lang = [];
+        foreach (LaravelLocalization::getSupportedLocales() as $key => $locale) {
+            $lang[$key] = $glossary->term;
+        }
+        $data['lang'] = $lang;
+        return $this->glossaryVariantRepository->store($data);
     }
 
     /**
