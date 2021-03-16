d3a9102c0047b29b2e041122725ce69a23d3a45f
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-03-16 17:17:38 +0100

    Add margin top to odd testimonials

--- resources/views/partials/pages/home/testimonials/block.blade.php
+++ resources/views/partials/pages/home/testimonials/block.blade.php
@@ -6,7 +6,10 @@
 
     <div class="lg:grid lg:grid-cols-3 lg:gap-4 px-4 lg:px-0 items-stretch justify-center">
         @foreach($testimonials as $testimonial)
-            @include('partials.pages.home.testimonials.block_testimonial')
+            <div class="@if($loop->iteration % 2 == 0) mt-8 @endif">
+                @include('partials.pages.home.testimonials.block_testimonial')
+            </div>
         @endforeach
     </div>
 </div>
+
