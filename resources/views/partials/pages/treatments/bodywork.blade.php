dec89f2cacd62b550ee47d429ac751c4803424c5
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-03-20 12:08:39 +0100

    Add animated SVG to treatments page

---
+++ resources/views/partials/pages/treatments/bodywork.blade.php
@@ -0,0 +1,9 @@
+<h2 class="text-xl text-gray-900 font-extrabold tracking-tight sm:text-2xl mt-8 mb-4">
+    Ilan Lev Method Bodywork
+</h2>
+
+<div class="">
+    During a session, the practitioner brings to the body of the receiver a wide variety of movements, vibrations, playful provocations. These create the environment in which the body's natural ability to heal itself can arise, we discover an optimal range of motion, efficiency of effort, and a new understanding of ourselves.
+    This allows new physical pathways and coordinations to emerge or become clearer, old habits to be thrown out, and physical and emotional trauma to be released.
+    In this way, the movement returns to parts of the body where communication was cut-off or stopped due to injury, pain, or emotional issues. And the dialogue within the whole system is restored.
+</div>
---
+++ resources/views/partials/pages/treatments/movementClasses.blade.php
@@ -0,0 +1,12 @@
+<div class="">
+    <h2 class="text-xl text-gray-900 font-extrabold tracking-tight sm:text-2xl mt-8 mb-4">
+        Movement classes according to the Ilan Lev Method
+    </h2>
+
+    <p>
+        The classes are made to improve the inner body dialogue and develop our functioning abilities out of the motion of inner listening and self pulsating.
+        In these group classes, the participants are laying on the floor, sitting or moving. The class process is refreshing, intriguing and increasing the body's vitality. Through various images, we learn to break through functional patterns that we have collected over the years, with the understanding that this is how the deepest learning takes place.
+
+        The activity is intended for all those who want to improve their functioning abilities.
+    </p>
+</div>
