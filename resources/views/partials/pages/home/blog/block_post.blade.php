b7001363beeef9a4d50da9fd321a0dd5ca32ec96
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-03-24 19:15:36 +0100

    Fix blog images in hp blog block

--- resources/views/partials/pages/home/blog/block_post.blade.php
+++ resources/views/partials/pages/home/blog/block_post.blade.php
@@ -1,7 +1,11 @@
 <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
 
     <div class="flex-shrink-0">
-        <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
+        @if($post->hasMedia('introimage'))
+            <img class="h-48 w-full object-cover" src="{{$post->getMedia('introimage')->first()->getUrl('thumb')}}?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
+        @else
+            <img class="h-48 w-full object-cover" src="https://dummyimage.com/640x360/bbb/ffffff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
+        @endif
     </div>
     <div class="flex-1 bg-white p-6 flex flex-col justify-between">
         <div class="flex-1">
