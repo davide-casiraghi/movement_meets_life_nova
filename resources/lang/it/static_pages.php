00cfe5bded72072089960b6a264ee93e172df3dc
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-03-28 18:15:00 +0200

    Fix italian translation of CI in hp

--- resources/lang/it/static_pages.php
+++ resources/lang/it/static_pages.php
@@ -18,14 +18,14 @@ return [
         'ilm' => [
           'ilan_lev_method' => 'ILAN LEV METHOD',
           'a_revolutionary_form' => "Una forma rivoluzionaria di <br><span class='text-primary-600'>bodywork</span> che affronta dolori muscolari, dolori e squilibri del movimento",
-          'through_bodywork' => "Attraverso il bodywork possiamo ottenere un supporto esterno per affrontare queste situazioni e per permettere al nostro corpo di ritrovare un nuovo equilibrio e allineamento libero da tensioni.",
+          'through_bodywork' => "Attraverso un bodywork possiamo ottenere un supporto esterno per affrontare queste situazioni e per permettere al nostro corpo di ritrovare un nuovo equilibrio e allineamento libero da tensioni.",
           'get_a_treatment' => 'Prenota un trattamento',
           'more_info' => 'Maggiori informazioni',
         ],
         'contact' => [
           'contact_improvisation' => 'CONTACT IMPROVISATION',
-          'more_than_a_dance' => "Più che una danza, come una <span class='text-primary-600'>pratica di mindfulness</span> risveglia la nostra agilità, scioglie tensioni e ci aiuta a ritrovare uno stato di pace interiore.",
-          'ci_is_liberating' => "La danza di Contact Improvisation è liberatoria, creativa e divertente. Si tratta di esplorare il movimento, l'equilibrio, il peso, il contatto fisico e la comunicazione, coinvolgendo due o più persone contemporaneamente.",
+          'more_than_a_dance' => "Più che una danza. E' come una <span class='text-primary-600'>pratica di mindfulness</span> che risveglia la nostra agilità, scioglie tensioni e ci aiuta a ritrovare uno stato di pace interiore.",
+          'ci_is_liberating' => "La danza di Contact Improvisation è liberatoria, creativa e divertente. Ci permette di esplorare il movimento, l'equilibrio, il peso, il contatto fisico e la relazione, coinvolgendo due o più persone contemporaneamente.",
           'more_about_ci' => 'Maggiori informazioni',
           ],
       ]
