2736c0b6e53d103f3a0098266999d2426d9ce672
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-03-31 08:08:48 +0200

    Removes descriptions from treatments gallery

--- resources/views/partials/pages/treatments/treatment_gallery.blade.php
+++ resources/views/partials/pages/treatments/treatment_gallery.blade.php
@@ -8,38 +8,24 @@
                 ],
                 2 => [
                   'name' => '2.jpg',
-                  'description' => 'description',
-                  'credits' => 'credits',
                 ],
                 3 => [
                   'name' => '3.jpg',
-                  'description' => 'description',
-                  'credits' => 'credits',
                 ],
                 4 => [
                   'name' => '4.jpg',
-                  'description' => 'description',
-                  'credits' => 'credits',
                 ],
                 5 => [
                   'name' => '5.jpg',
-                  'description' => 'description',
-                  'credits' => 'credits',
                 ],
                 6 => [
                   'name' => '6.jpg',
-                  'description' => 'description',
-                  'credits' => 'credits',
                 ],
                 7 => [
                   'name' => '7.jpg',
-                  'description' => 'description',
-                  'credits' => 'credits',
                 ],
                 8 => [
                   'name' => '8.jpg',
-                  'description' => 'description',
-                  'credits' => 'credits',
                 ],
             ]
         ])
