26f0e920d475e57ce4a6f085e4396bf479c6138c
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-04-05 18:06:14 +0200

    Removes the bg-fixed from the blog post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -42,7 +42,7 @@
 
 @section('jumbotron')
     @if($post->hasMedia('introimage'))
-        <div class="bg-fixed relative bg-cover bg-no-repeat" style="background-image: url('{{$post->getMedia('introimage')[0]->getUrl()}}'); ">
+        <div class="{{--bg-fixed--}} relative bg-cover bg-no-repeat" style="background-image: url('{{$post->getMedia('introimage')[0]->getUrl()}}'); ">
             <div class="container mx-auto px-6 py-40 max-w-prose relative z-10">
                 <h2 class="text-4xl font-bold mb-2 text-white">
                     {{ $post->title }}
