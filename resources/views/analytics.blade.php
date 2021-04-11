cc334db8f833121efbe58f77102e38a33a3b93ca
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-04-11 14:13:47 +0200

    Show analytics just in prod

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -1,5 +1,5 @@
 {{-- Global site tag (gtag.js) - Google Analytics --}}
-@production
+@env('production')
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-E8J8KSDTGD"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -8,4 +8,4 @@
 
         gtag('config', 'G-E8J8KSDTGD');
     </script>
-@endproduction
+@endenv
