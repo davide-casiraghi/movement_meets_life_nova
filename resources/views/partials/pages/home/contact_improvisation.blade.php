f23a28dde4ab074110900e4667613352692d0a25
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-04-11 12:50:44 +0200

    Add CI video in homepage

--- resources/views/partials/pages/home/contact_improvisation.blade.php
+++ resources/views/partials/pages/home/contact_improvisation.blade.php
@@ -51,9 +51,21 @@
             {!! $videoIntro !!}--}}
 
             <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
-                <img class="w-full" src="{{asset('images/static_pages/hp/contact_improvisation_small.jpg')}}" alt="Woman making a sale">
-                {{--<iframe width="450" height="300" src="https://www.youtube.com/embed/MelWoO9J3E8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>--}}
+                <a data-fancybox href="https://www.youtube.com/embed/MelWoO9J3E8">
+                    <img class="w-full" src="{{asset('images/static_pages/hp/contact_improvisation_small.jpg')}}" alt="Woman making a sale">
+                    {{--<iframe width="450" height="300" src="https://www.youtube.com/embed/MelWoO9J3E8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>--}}
+                    <div class="absolute inset-0 w-full h-full flex items-center justify-center">
+                        <svg class="h-20 w-20 text-primary-500" fill="currentColor" viewBox="0 0 84 84">
+                            <circle opacity="0.9" cx="42" cy="42" r="42" fill="white" />
+                            <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
+                        </svg>
+                    </div>
+                </a>
+
             </div>
+
+
+
 
 </div>
 </div>
