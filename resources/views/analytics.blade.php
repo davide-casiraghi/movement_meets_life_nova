6139038493de6db0e1b89148bc1a38d99ae88887
Davide Casiraghi <davide.casiraghi@agiledrop.com>
2021-04-12 10:18:51 +0200

    Show Analytics just in production

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -1,9 +1,11 @@
 {{-- Global site tag (gtag.js) - Google Analytics --}}
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-E8J8KSDTGD"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+@env('production')
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-E8J8KSDTGD"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'G-E8J8KSDTGD');
-</script>
+        gtag('config', 'G-E8J8KSDTGD');
+    </script>
+@endenv
