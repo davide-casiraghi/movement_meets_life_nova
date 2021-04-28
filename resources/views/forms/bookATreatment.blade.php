54d2cf34d6f7c30ad80baf59fe0f6d94dfee3285
Davide Casiraghi <davide.casiraghi@agiledrop.com>
2021-04-28 10:42:50 +0200

    Add title and description to book a treatment page

--- resources/views/forms/bookATreatment.blade.php
+++ resources/views/forms/bookATreatment.blade.php
@@ -6,6 +6,16 @@
 
     @include('partials.messages')
 
+        <div class="text-center mt-5 px-4 md:px-0">
+            <h2 class="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
+                Book a treatment
+            </h2>
+            <p class="mt-3 max-w-2xl mx-auto text-xl leading-7 text-gray-500 sm:mt-4">
+                To book a treatment, please select one of the available time frames. <br>
+                If no one fits your schedule, please <a class="textLink" href="contact" >contact me</a>, and we will find a solution.
+            </p>
+        </div>
+
         <!-- Calendly inline widget begin -->
         <div class="calendly-inline-widget" style="position:relative; min-width:320px; height:750px;" data-auto-load="false">
             <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
