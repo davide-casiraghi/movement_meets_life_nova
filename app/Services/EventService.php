0912b6fd467c6c4224c9508fbe1949682e3c9103
Enrico Colombo <enrico.colombo@gmail.com>
2021-06-10 16:38:00 +0200

    Sort past events in descending order, from newest to oldest

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -108,14 +108,14 @@ class EventService
     /**
      * Get all the Events.
      *
-     * @param int|null $recordsPerPage
-     * @param array|null $searchParameters
-     *
+     * @param  int|null  $recordsPerPage
+     * @param  array|null  $searchParameters
+     * @param  string  $orderDirection sorting direction: 'asc' = from oldest to newest | 'desc' = from newest to oldest
      * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Contracts\Pagination\LengthAwarePaginator
      */
-    public function getEvents(int $recordsPerPage = null, array $searchParameters = null)
+    public function getEvents(int $recordsPerPage = null, array $searchParameters = null, string $orderDirection = 'asc')
     {
-        return $this->eventRepository->getAll($recordsPerPage, $searchParameters);
+        return $this->eventRepository->getAll($recordsPerPage, $searchParameters, $orderDirection);
     }
 
     /**
