bd417562005a609721127d43fa0888bdc5ca45a3
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-10-23 13:04:52 +0200

    Makes the testimonial carousel responsive

--- resources/js/vendors/slick_carousel.js
+++ resources/js/vendors/slick_carousel.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-    $('.testimonialsList').slick({
+
+
+/*$(document).ready(function(){
+
+    // Create carousel
+    $('.testimonialsList').not('.slick-initialized').slick({
         dots: true,
         arrows: true,
         infinite: true,
@@ -10,3 +14,71 @@ $(document).ready(function(){
         pauseOnHover: true
     });
 });
+
+ */
+
+
+
+// Create carousel
+function createTestimonialCarousel(numberOfSlides){
+    jQuery('.testimonialsList').not('.slick-initialized').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: numberOfSlides,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        pauseOnHover: true
+    });
+}
+
+// Calculate number of slides to show
+function calculateNumberOfSlidesToShow(){
+    var carouselWidth = jQuery(".testimonialsList").width();
+
+    var numberOfSlides = 0;
+    switch (true) {
+        case (carouselWidth < 767):
+            numberOfSlides = 1;
+            break;
+        case (carouselWidth < 991):
+            numberOfSlides = 2;
+            break;
+        case (carouselWidth < 1199):
+            numberOfSlides = 3;
+            break;
+        case (carouselWidth > 1200):
+            numberOfSlides = 3;
+            break;
+    }
+
+    return numberOfSlides;
+}
+
+// Reload Carousel on browser resize (to make it responsible)
+
+function reloadCarousel () {
+    jQuery('.testimonialsList').slick('unslick');
+    numberOfSlides = calculateNumberOfSlidesToShow();
+    createTestimonialCarousel(numberOfSlides);
+}
+
+// Call updateMaxHeight when browser resize event fires
+jQuery(window).on("resize", reloadCarousel);
+
+
+
+jQuery(document).ready(function () {
+
+    // Start carousel
+    if (jQuery(".testimonialsList").length) {
+        setTimeout(function () {
+            numberOfSlides = calculateNumberOfSlidesToShow();
+            createTestimonialCarousel(numberOfSlides);
+        }, 300);
+    }
+
+
+});
+
--- resources/views/partials/pages/home/testimonials/slick/block_testimonial.blade.php
+++ resources/views/partials/pages/home/testimonials/slick/block_testimonial.blade.php
@@ -1,12 +1,12 @@
 <div class="mb-10 lg:mb-0">
     <blockquote class="col-span-3 md:col-span-1 rounded-md shadow-xl overflow-hidden bg-white">
-        <div class="flex-grow flex items-center px-12 py-6 text-xl leading-relaxed text-gray-500">
+        <div class="flex-grow flex items-center p-6 text-lg leading-relaxed text-gray-500">
             <p class="">
                 {{$testimonial->feedback_short}}
             </p>
         </div>
 
-        <footer class="z-20 px-12 py-3 bg-gray-200">
+        <footer class="z-20 px-6 py-3 bg-gray-200">
             <a href="{{route('testimonials.show', $testimonial->id)}}" target="_blank" rel="noopener" class="flex items-center group">
                     <span class="mr-3 w-12 h-12 rounded-full overflow-hidden shadow">
                         @if($testimonial->getMedia('photo')->first())
