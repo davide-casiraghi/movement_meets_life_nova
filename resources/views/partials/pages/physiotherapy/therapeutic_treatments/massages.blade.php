8b43b2640a4404213e4b627d56b2b0e3a3920f28
Davide Casiraghi <davide.casiraghi@gmail.com>
2026-03-09 19:02:46 +0100

    add massages_description to theraphy session

--- resources/views/partials/pages/physiotherapy/therapeutic_treatments/massages.blade.php
+++ resources/views/partials/pages/physiotherapy/therapeutic_treatments/massages.blade.php
@@ -1,1 +1,23 @@
-bbb
+
+<div class="mt-8 mx-auto max-w-screen-xl p-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24">
+    <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+        <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+            
+            <h3 class="mt-1 text-4xl tracking-tight leading-10 font-brand text-gray-900 sm:leading-none sm:text-6xl lg:text-4xl xl:text-5xl">
+                @lang('static_pages.physiotherapy.offers.massages')
+            </h3>
+            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                @lang('static_pages.physiotherapy.offers.massages_description')
+            </p>
+            
+            
+        </div>
+        <div class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+        
+            <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+                <img class="w-full" src="{{asset('images/static_pages/hp/bodywork_color.jpeg')}}" alt="Davide Casiraghi giving Ilan Lev method bodywork">
+            </div>
+
+        </div>
+    </div>
+</div>
